154c0348ecb5015e7880cad85948b14cff98887b
Mehdi Benjelloun <mehdibenjellouncoding@gmail.com>
2025-07-03 00:36:46 +0100

    Add

--- script.js
+++ script.js
@@ -16,8 +16,10 @@ fetch('coran.json')
       .normalize("NFD")
       .replace(/[\u0610-\u061A\u064B-\u065F\u0670\u06D6-\u06ED]/g, '') // chakl
       .replace(/ٱ/g, 'ا')      // alif wasl
-      .replace(/ـٰ/g, '')      // madd avec tatweel
-      .replace(/ـ/g, '');      // tatweel seul
+      .replace(/ـٰ/g, '')      // madd
+      .replace(/ـ/g, '')       // tatweel
+      .replace(/\u200C|\u200D|\u200E|\u200F|\u202A-\u202E/g, '') // zero-width/invisibles
+      .replace(/\s+/g, '');    // tous les espaces visibles et invisibles
   }
   
   
@@ -33,6 +35,9 @@ function displayVerses(searchTerm) {
     for (const verset in coran[sourate]) {
       const entry = coran[sourate][verset];
       const normalizedText = removeDiacritics(entry.ar);
+      console.log("Texte normalisé :", removeDiacritics(entry.ar));
+console.log("Recherche normalisée :", removeDiacritics(searchTerm));
+
 
       if (!normalizedSearch || normalizedText.includes(normalizedSearch)) {
         const div = document.createElement('div');
